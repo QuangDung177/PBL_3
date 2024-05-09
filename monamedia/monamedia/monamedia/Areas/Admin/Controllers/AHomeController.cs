35467f1407c9a3af258083ae4cb1270a939312a3
QuangDung177 <nguyenquangdung177@gmail.com>
2024-05-09 17:10:19 +0700

    admin update

--- monamedia/monamedia/monamedia/Areas/Admin/Controllers/AHomeController.cs
+++ monamedia/monamedia/monamedia/Areas/Admin/Controllers/AHomeController.cs
@@ -10,32 +10,92 @@ using System.Web.Mvc;
 
 namespace monamedia.Areas.Admin.Controllers
 {
-    
+
     public class AHomeController : Controller
     {
         // GET: Admin/AHome
-        public ActionResult Index(string search="")
+        public ActionResult Index(string search = "")
         {
             AppDbContext db = new AppDbContext();
-            List<monamedia.Models.Staff> Staffs=db.Staffs.Where(row => row.fullName.Contains(search)).ToList();
-            ViewBag.Search= search;
+            List<monamedia.Models.Staff> Staffs = db.Staffs.Where(row => row.fullName.Contains(search)).ToList();
+            ViewBag.Search = search;
             return View(Staffs);
+        }
+        public ActionResult Information(int idManager, int idAccount)
+        {
+            AppDbContext db = new AppDbContext();
+            monamedia.Models.Manager Managers = db.Managers.Where(row => row.managerID == idManager).FirstOrDefault();
+            monamedia.Models.Account Accounts = db.Accounts.Where(row => row.accountID == idAccount).FirstOrDefault();
+            var viewModel = new monamedia.Areas.Admin.Data.EditInformation
+            {
+                Manager = Managers,
+                Account = Accounts
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        public ActionResult Information(monamedia.Models.Manager m, monamedia.Models.Account a)
+        {
+            AppDbContext db = new AppDbContext();
+            monamedia.Models.Manager Managers = db.Managers.Where(row => row.managerID == m.managerID).FirstOrDefault();
+            monamedia.Models.Account Accounts = db.Accounts.Where(row => row.accountID == a.accountID).FirstOrDefault();
+            //Update
+            //account
+            Accounts.userName = a.userName;
+            Accounts.password = a.password;
+            Accounts.email = a.email;
+            Accounts.role = a.role;
+            //Staff
+            Managers.phoneNumber = m.phoneNumber;
+            Managers.address = m.address;
+            Managers.birthDate = m.birthDate;
+            Managers.experience = m.experience;
+            Managers.gender = m.gender;
+            Managers.fullName = m.fullName;
+            db.SaveChanges();
+            return RedirectToAction("Information", new { idManager = m.managerID, idAccount = a.accountID });
+        }
+
+        public ActionResult AccountManage(string search = "", string filter = "")
+        {
+            AppDbContext db = new AppDbContext();
+            List<monamedia.Models.Account> Accounts;
+
+            switch (filter)
+            {
+                case "guest":
+                    // Tìm kiếm trong danh sách khách hàng
+                    Accounts = db.Accounts.Where(row => row.userName.Contains(search) && row.role == "Guest").ToList();
+                    break;
+                case "staff":
+                    // Tìm kiếm trong danh sách nhân viên
+                    Accounts = db.Accounts.Where(row => row.userName.Contains(search) && row.role == "Staff").ToList();
+                    break;
+                default:
+                    // Tìm kiếm trong toàn bộ danh sách
+                    Accounts = db.Accounts.Where(row => row.userName.Contains(search)).ToList();
+                    break;
+            }
+
+            ViewBag.Search = search;
+            return View(Accounts);
         }
         public ActionResult CreateStaff()
         {
             return View();
         }
         [HttpPost]
-        public ActionResult CreateStaff(monamedia.Models.Staff s,monamedia.Models.Account a) {
+        public ActionResult CreateStaff(monamedia.Models.Staff s, monamedia.Models.Account a)
+        {
             AppDbContext db = new AppDbContext();
             db.Accounts.Add(a);
             db.SaveChanges();
-            s.accountID= a.accountID;
+            s.accountID = a.accountID;
             db.Staffs.Add(s);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-        public ActionResult EditStaff(int idStaff,int idAccount)
+        public ActionResult EditStaff(int idStaff, int idAccount)
         {
             AppDbContext db = new AppDbContext();
             monamedia.Models.Staff Staffs = db.Staffs.Where(row => row.staffID == idStaff).FirstOrDefault();
@@ -49,28 +109,28 @@ namespace monamedia.Areas.Admin.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult EditStaff(monamedia.Models.Staff s,monamedia.Models.Account a)
+        public ActionResult EditStaff(monamedia.Models.Staff s, monamedia.Models.Account a)
         {
             AppDbContext db = new AppDbContext();
             monamedia.Models.Staff Staffs = db.Staffs.Where(row => row.staffID == s.staffID).FirstOrDefault();
             monamedia.Models.Account Accounts = db.Accounts.Where(row => row.accountID == a.accountID).FirstOrDefault();
-           //Update
-           //account
-           Accounts.userName= a.userName;
-            Accounts.password= a.password;
-            Accounts.email= a.email;
-            Accounts.role= a.role;
+            //Update
+            //account
+            Accounts.userName = a.userName;
+            Accounts.password = a.password;
+            Accounts.email = a.email;
+            Accounts.role = a.role;
             //Staff
-            Staffs.phoneNumber=s.phoneNumber;
-            Staffs.address=s.address;
-            Staffs.birthDate=s.birthDate;
-            Staffs.salary=s.salary;
-            Staffs.gender=s.gender;
-            Staffs.fullName=s.fullName;
+            Staffs.phoneNumber = s.phoneNumber;
+            Staffs.address = s.address;
+            Staffs.birthDate = s.birthDate;
+            Staffs.salary = s.salary;
+            Staffs.gender = s.gender;
+            Staffs.fullName = s.fullName;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-        public ActionResult DeleteStaff(string selectedStaffIds,string selectedAccountIds)
+        public ActionResult DeleteStaff(string selectedStaffIds, string selectedAccountIds)
         {
             AppDbContext db = new AppDbContext();
             var Staffids = selectedStaffIds.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries)
@@ -107,7 +167,7 @@ namespace monamedia.Areas.Admin.Controllers
                 db.Staffs.Remove(Staffs);
                 db.SaveChanges();
             }
-           
+
             return RedirectToAction("Index");
         }
 
